a026d687a2fd14d6ad9255e642a32b23f3af800d
khanempire <mdaamirkhan7186@gmail.com>
2021-08-08 22:23:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const bg = document.querySelector(".bg");
+const loading = document.querySelector(".loading");
+
+let load = 0;
+let blurr = 0;
+
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(interval);
+  }
+  loading.innerText = `${load}%`;
+  loading.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
